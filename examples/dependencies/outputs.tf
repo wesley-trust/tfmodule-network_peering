51ac9852e5416122218167bf67f803598906e41a
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2022-05-25 21:07:59 +0100

    Test

--- examples/dependencies/outputs.tf
+++ examples/dependencies/outputs.tf
@@ -1,4 +1,4 @@
-output "service_network_spoke" {
+/* output "service_network_spoke" {
   value = {
     "network_name"        = values(module.service_network_spoke)[*].network_name
     "resource_group_name" = values(module.resource_group_spoke)[*].name
@@ -9,5 +9,19 @@ output "service_network_hub" {
   value = {
     "network_name"        = values(module.service_network_hub)[*].network_name
     "resource_group_name" = values(module.resource_group_hub)[*].name
+  }
+} */
+
+output "service_network_spoke" {
+  value = {
+    "network_name"        = module.service_network_spoke[*].network_name
+    "resource_group_name" = module.resource_group_spoke[*].name
+  }
+}
+
+output "service_network_hub" {
+  value = {
+    "network_name"        = module.service_network_spoke[*].network_name
+    "resource_group_name" = module.resource_group_spoke[*].name
   }
 }
