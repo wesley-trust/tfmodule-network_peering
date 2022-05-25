39027ed49c0521bd6e4059fbc76800632238b875
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2022-05-25 20:32:54 +0100

    Test

--- examples/resources.tf
+++ examples/resources.tf
@@ -2,7 +2,7 @@ module "network_peering_spoke" {
   count                      = length(var.service_location)
   source                     = "../"
   service_environment        = var.service_environment
-  resource_network_peer      = var.service_network_spoke[count.index]
+  resource_network_peer      = var.service_network_spoke
   resource_network_peer_role = "hub"
 }
 
@@ -10,6 +10,6 @@ module "network_peering_hub" {
   count                      = length(var.service_location)
   source                     = "../"
   service_environment        = var.service_environment
-  resource_network_peer      = var.service_network_hub[count.index]
+  resource_network_peer      = var.service_network_hub
   resource_network_peer_role = "spoke"
 }
