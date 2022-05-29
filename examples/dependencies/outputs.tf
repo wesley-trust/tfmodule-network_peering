a35765573b4d9250bc21415707347f02101029c7
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2022-05-29 13:26:58 +0100

    Testing

--- examples/dependencies/outputs.tf
+++ examples/dependencies/outputs.tf
@@ -27,9 +27,9 @@ output "service_network_hub" {
 } */
 
 output "service_network_spoke" {
-  value = module.service_network_spoke[*]
+  value = module.service_network_spoke
 }
 
 output "service_network_hub" {
-  value = module.service_network_hub[*]
+  value = module.service_network_hub
 }
--- examples/resources.tf
+++ examples/resources.tf
@@ -1,17 +1,17 @@
 module "network_peering_spoke" {
-  count                      = length(var.service_location)
+  for_each                   = var.service_location
   source                     = "../"
   service_environment        = var.service_environment
-  resource_network_peer      = var.service_network_spoke[count.index].network_name
-  resource_group_peer        = var.service_network_spoke[count.index].resource_group_name
+  resource_network_peer      = var.service_network_hub[each.value].network_name
+  resource_group_peer        = var.service_network_hub[each.value].resource_group_name
   resource_network_peer_role = "hub"
 }
 
 module "network_peering_hub" {
-  count                      = length(var.service_location)
+  for_each                   = var.service_location
   source                     = "../"
   service_environment        = var.service_environment
-  resource_network_peer      = var.service_network_hub[count.index].network_name
-  resource_group_peer        = var.service_network_hub[count.index].resource_group_name
+  resource_network_peer      = var.service_network_hub[each.value].network_name
+  resource_group_peer        = var.service_network_hub[each.value].resource_group_name
   resource_network_peer_role = "spoke"
 }
