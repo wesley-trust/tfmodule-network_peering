1899a670c37cf283cb8a6d15e140b7d5083b47f4
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2022-05-29 15:38:20 +0100

    Update

--- examples/dependencies/outputs.tf
+++ examples/dependencies/outputs.tf
@@ -1,31 +1,3 @@
-/* output "service_network_spoke" {
-  value = {
-    "network_name"        = values(module.service_network_spoke)[*].network_name
-    "resource_group_name" = values(module.resource_group_spoke)[*].name
-  }
-}
-
-output "service_network_hub" {
-  value = {
-    "network_name"        = values(module.service_network_hub)[*].network_name
-    "resource_group_name" = values(module.resource_group_hub)[*].name
-  }
-} */
-
-/* output "service_network_spoke" {
-  value = {
-    "networkName"       = module.service_network_spoke[*].network_name
-    "resourceGroupName" = module.service_network_spoke[*].resource_group_name
-  }
-}
-
-output "service_network_hub" {
-  value = {
-    "networkName"       = module.service_network_hub[*].network_name
-    "resourceGroupName" = module.service_network_hub[*].resource_group_name
-  }
-} */
-
 output "service_network_spoke" {
   value = module.service_network_spoke
 }
--- examples/dependencies/resources_spoke.tf
+++ examples/dependencies/resources_spoke.tf
@@ -18,24 +18,3 @@ module "service_network_spoke" {
   resource_address_space  = lookup(lookup(var.resource_address_space, "Spoke", null), each.value, null)
   resource_network_role   = "spoke"
 }
-
-module "resource_group_hub" {
-  for_each            = toset(var.service_location)
-  source              = "github.com/wesley-trust/tfmodule-resource_group"
-  service_environment = var.service_environment
-  service_name        = var.service_name
-  service_location    = each.value
-  service_deployment  = "${var.service_deployment}-Hub"
-}
-
-module "service_network_hub" {
-  for_each                = toset(var.service_location)
-  source                  = "github.com/wesley-trust/tfsubmodule-network"
-  service_name            = "${var.service_name}-Hub"
-  service_location_prefix = replace(each.value, "/[a-z[:space:]]/", "")
-  resource_location       = module.resource_group_hub[each.value].location
-  resource_group_name     = module.resource_group_hub[each.value].name
-  resource_environment    = var.service_environment
-  resource_address_space  = lookup(lookup(var.resource_address_space, "Hub", null), each.value, null)
-  resource_network_role   = "hub"
-}
